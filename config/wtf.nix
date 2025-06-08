823039f3ca1dc6a5e9c9fcdc08eb66dd33153843
xhuyz <huylovengoc35@gmail.com>
2025-06-08 02:35:42 +0000

    keymap for wtf

--- config/wtf.nix
+++ config/wtf.nix
@@ -1,6 +1,35 @@
 {
   plugins.wtf = {
     enable = true;
-    autoLoad = true;
   };
+  keymaps = [
+    {
+      key = "<leader>wa";
+      mode = [
+        "n"
+      ];
+      action = "<cmd>Wtf<CR>";
+    }
+    {
+      key = "<leader>ws";
+      mode = [
+        "n"
+      ];
+      action = "<cmd>WtfSearch google<CR>";
+    }
+    {
+      key = "<leader>wh";
+      mode = [
+        "n"
+      ];
+      action = "<cmd>WtfHistory<CR>";
+    }
+    {
+      key = "<leader>wg";
+      mode = [
+        "n"
+      ];
+      action = "<cmd>WtfGrepHistory<CR>";
+    }
+  ];
 }
