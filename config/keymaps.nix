ed532d503134a460a9e384e4f739ce04fd0a6a3e
xhuyz <huylovengoc35@gmail.com>
2025-06-08 01:36:53 +0000

    add keymap in the same .nix for toggleterm and yazi

--- config/keymaps.nix
+++ config/keymaps.nix
@@ -2,18 +2,6 @@
 {
   keymaps = [
     {
-      key = "<C-t>";
-      mode = [
-        "n"
-        "t"
-      ];
-      action = "<cmd>ToggleTerm<CR>";
-      options = {
-        silent = true;
-        noremap = true;
-      };
-    }
-    {
       key = "<leader>gg";
       mode = [ "n" ];
       action = "<cmd>LazyGit<CR>";
--- config/yazi.nix
+++ config/yazi.nix
@@ -10,16 +10,12 @@
       yazi_floating_window_border = "single";
       yazi_floating_window_winblend = 50;
     };
-    keymaps = {
-      show_help = "<f1>";
-      open_file_in_vertical_split = "<c-v>";
-      open_file_in_horizontal_split = "<c-x>";
-      open_file_in_tab = "<c-ft>";
-      grep_in_directory = "<c-s>";
-      replace_in_directory = "<c-g>";
-      cycle_open_buffers = "<tab>";
-      copy_relative_path_to_selected_files = "<c-y>";
-      send_to_quickfix_list = "<c-q>";
-    };
   };
+  keymaps = [
+    {
+      key = "<leader>fm";
+      mode = [ "n" ];
+      action = "<cmd>Yazi<CR>";
+    }
+  ];
 }
