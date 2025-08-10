57decfff74a8c7feda6e60673138cc3afd94206a
xhuyz <huylovengoc35@gmail.com>
2025-08-10 23:53:38 +0700

    update with metaData

--- flake.nix
+++ flake.nix
@@ -1,12 +1,10 @@
 {
   description = "xhuyz nixvim configuration";
-
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     nixvim.url = "github:nix-community/nixvim";
   };
-
   outputs = { self, nixpkgs, nixpkgs-unstable, nixvim, ... }:
     let
       system = "x86_64-linux";
@@ -16,22 +14,34 @@
         inherit pkgs;
         module = config;
       };
+      
+      # Wrap với metadata cần thiết
+      nvimWithMeta = nvim.overrideAttrs (oldAttrs: {
+        meta = (oldAttrs.meta or {}) // {
+          description = "Neovim configured with nixvim";
+          longDescription = "Custom Neovim configuration built with nixvim";
+          homepage = "https://github.com/XhuyZ/nixvim";
+          license = pkgs.lib.licenses.mit;
+          maintainers = [ ];
+          platforms = pkgs.lib.platforms.unix;
+        };
+      });
     in
     {
-      # Export package bình thường
-      packages.${system}.default = nvim;
-
+      # Export package với metadata
+      packages.${system}.default = nvimWithMeta;
+      
       # Export overlay để flake khác có thể dùng
       overlays.default = final: prev: {
-        nixvim-xhuyz = nvim;
+        nixvim-xhuyz = nvimWithMeta;
       };
-
+      
       # Check
       checks.${system}.default = nixvim.lib.${system}.check.mkTestDerivationFromNvim {
-        inherit nvim;
+        nvim = nvimWithMeta;
         name = "A nixvim configuration";
       };
-
+      
       # Dev shell
       devShells.${system}.default = pkgs.mkShellNoCC {
         shellHook = ''
@@ -40,11 +50,10 @@
           alias vim='nvim'
         '';
         packages = [
-          nvim
+          nvimWithMeta
           pkgs.dotnet-sdk
           pkgs.dotnet-aspnetcore
         ];
       };
     };
 }
-
