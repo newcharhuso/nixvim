05cc2a19887962f9f24c800d66053747ddc72619
xhuyz <huylovengoc35@gmail.com>
2025-08-10 22:47:56 +0700

    add overlays for nixvim

--- flake.nix
+++ flake.nix
@@ -7,31 +7,32 @@
     nixvim.url = "github:nix-community/nixvim";
   };
 
-  outputs =
-    {
-      nixpkgs,
-      nixpkgs-unstable,
-      nixvim,
-      ...
-    }:
+  outputs = { self, nixpkgs, nixpkgs-unstable, nixvim, ... }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
       config = import ./config;
-
       nvim = nixvim.legacyPackages.${system}.makeNixvimWithModule {
         inherit pkgs;
         module = config;
       };
     in
     {
+      # Export package bình thường
       packages.${system}.default = nvim;
 
+      # Export overlay để flake khác có thể dùng
+      overlays.default = final: prev: {
+        nixvim-xhuyz = nvim;
+      };
+
+      # Check
       checks.${system}.default = nixvim.lib.${system}.check.mkTestDerivationFromNvim {
         inherit nvim;
         name = "A nixvim configuration";
       };
 
+      # Dev shell
       devShells.${system}.default = pkgs.mkShellNoCC {
         shellHook = ''
           echo Welcome to a Neovim dev environment powered by Nixvim
@@ -42,8 +43,8 @@
           nvim
           pkgs.dotnet-sdk
           pkgs.dotnet-aspnetcore
-          pkgs.nodePackages_latest.nodejs
         ];
       };
     };
 }
+
